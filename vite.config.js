ca51b804459d935af5a5d197ab192f64f0b32503
Andrew <andrew@andrewd1.dev>
2026-06-12 13:54:16 -0700

    fix: make site base url work

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { router } from "sv-router/vite-plugin";
 
+const base = 'gh-issues-site-poc'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
-    router(),
+    router({
+      base,
+    }),
   ],
-  base: '/gh-issues-site-poc/',
+  base: `/${base}/`,
 })
